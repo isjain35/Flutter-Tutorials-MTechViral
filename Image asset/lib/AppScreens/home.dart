74aeac757d03f29e6facf48b2ecd8610bcaaf397
unknown <isjain35@gmail.com>
2019-07-10 23:09:53 +0530

    Image implementation in app

---
+++ Image asset/lib/AppScreens/home.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+          padding: EdgeInsets.only(left: 10.0, top: 50.0),
+          alignment: Alignment.center,
+          color: Colors.deepPurple,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Spice Jet",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 35.0,
+                        fontFamily: 'Kerb',
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.normal,
+                        color: Colors.white),
+                  )),
+                  Expanded(
+                      child: Text(
+                    "From Delhi to Mumbai via Banglore",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 20.0,
+                        fontFamily: 'Kerb',
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.normal,
+                        color: Colors.white),
+                  )),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Emirates",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 35.0,
+                        fontFamily: 'Kerb',
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.normal,
+                        color: Colors.white),
+                  )),
+                  Expanded(
+                      child: Text(
+                    "From Dubai to America via India",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 20.0,
+                        fontFamily: 'Kerb',
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.normal,
+                        color: Colors.white),
+                  )),
+                ],
+              ),
+              FlightImage(),
+            ],
+          )
+          ),
+    );
+  }
+}
+
+class FlightImage extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/plane.png');
+    Image image = Image(image: assetImage, width: 250.0, height: 250.0, color: Colors.white,);
+    return Container(child: image,);
+  }
+
+}
