9d47c2dc67d5f927cf826efe45b6090032c22979
unknown <isjain35@gmail.com>
2019-07-29 17:45:22 +0530

    For SI calculator app

---
+++ Logic adding and event handeling/lib/main.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Simple interest calculator",
+    home: SIForm(),
+    theme: ThemeData(
+      primaryColor: Colors.indigo,
+      accentColor: Colors.indigoAccent,
+      brightness: Brightness.dark,
+      buttonColor: Colors.orange,
+    ),
+  ));
+}
+
+class SIForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SIFormState();
+  }
+}
+
+class _SIFormState extends State<SIForm> {
+  var _currencies = ['Rupees', 'Dollars', 'Pounds'];
+  final _minimumPadding = 5.0;
+  var _currentItemSelected = '';
+  @override
+  void initState() {
+    super.initState();
+    _currentItemSelected = _currencies[0];
+  }
+  var result = '';
+  TextEditingController principalController = TextEditingController();
+  TextEditingController roiController = TextEditingController();
+  TextEditingController termController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    TextStyle textStyle = Theme.of(context).textTheme.title;
+
+    return Scaffold(
+      //resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text("Simple Interest Calculator"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
+        child: ListView(
+          children: <Widget>[
+            getImageMoney(),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextField(
+                  style: textStyle,
+                  controller: principalController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Principal amount",
+                    hintText: "Enter amount eg. 24000",
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextField(
+                  style: textStyle,
+                  controller: roiController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Rate of Interest",
+                    hintText: "In percentage",
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: TextField(
+                          style: textStyle,
+                          controller: termController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "Term",
+                        hintText: "Time in Years",
+                        labelStyle: textStyle,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                    )),
+                    Container(
+                      width: _minimumPadding * 5,
+                    ),
+                    Expanded(
+                        child: DropdownButton<String>(
+                      items: _currencies.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value,style: textStyle,),
+                        );
+                      }).toList(),
+                      value: _currentItemSelected,
+                      onChanged: (String newValueSelected) {
+                        setState(() {
+                          this._currentItemSelected = newValueSelected;
+                        });
+                      },
+                    )),
+                  ],
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: RaisedButton(
+                            child: Text("Calculate",textScaleFactor: 1.5,),
+                            onPressed: () {
+                              setState(() {
+                                this.result = _calculateTotalReturns();
+                              });
+                            })),
+                    Expanded(
+                        child: RaisedButton(
+                            color: Theme.of(context).primaryColorDark,
+                            textColor: Theme.of(context).primaryColor,
+                            child: Text("Reset",textScaleFactor: 1.5),
+                            onPressed: () {
+                              setState(() {
+                                reset();
+                              });
+                            }))
+                  ],
+                )),
+            Padding(
+                padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+              child: Text("$result",style: textStyle,),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getImageMoney() {
+    AssetImage assetImage = AssetImage("images/money.png");
+    Image image = Image(
+      image: assetImage,
+      width: 125.0,
+      height: 125.0,
+      color: Colors.green,
+    );
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_minimumPadding * 10),
+    );
+  }
+  String _calculateTotalReturns()
+  {
+    double principal = double.parse(principalController.text);
+    double roi = double.parse(roiController.text);
+    double term = double.parse(termController.text);
+    double payableAmount = principal + (principal * roi * term) / 100;
+    return "Total amount payable after $term years would be $payableAmount $_currentItemSelected";
+  }
+  void reset()
+  {
+    principalController.text = '';
+    roiController.text = '';
+    termController.text = '';
+    result = '';
+    _currentItemSelected = _currencies[0];
+  }
+}
